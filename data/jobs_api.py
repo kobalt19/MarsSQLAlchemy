a222c296a8a4a686789192999a011bc70583df1b
stepan <stepanmol4anoff@yandex.ru>
2023-04-30 02:28:23 +0300

    Modified jobs api

--- data/jobs_api.py
+++ data/jobs_api.py
@@ -12,3 +12,13 @@ def get_all_jobs():
     return jsonify({'jobs': [job.to_dict(
         only=('team_leader', 'job', 'work_size', 'start_date', 'end_date', 'is_finished')
     ) for job in all_jobs]})
+
+
+@api.route('/api/jobs/<int:id_>')
+def get_one_job(id_):
+    session = db_session.create_session()
+    job = session.get(Jobs, id_)
+    if not job:
+        return jsonify({'error': 'Not found'})
+    return jsonify({'job': job.to_dict(
+        only=('team_leader', 'job', 'work_size', 'start_date', 'end_date', 'is_finished'))})
