10f79909d21a6c555fc495231251864ed8a2b8f1
stepan <stepanmol4anoff@yandex.ru>
2023-04-30 13:56:44 +0300

    modified test and api

--- test.py
+++ test.py
@@ -4,7 +4,9 @@ import requests
 print('1:', requests.get('http://127.0.0.1:8080/api/jobs').json())
 print('2:', requests.get('http://127.0.0.1:8080/api/jobs/1').json())
 print('3:', requests.get('http://127.0.0.1:8080/api/jobs/555').json())
-# print('4:', requests.get('http://127.0.0.1:8080/api/jobs/etbt').json())
-print('5:', requests.post('http://127.0.0.1:8080/api/jobs', json={'team_leader': 3, 'job': 'job', 'work_size': 1,
-                                                                  'start_date': '2022-03-21', 'end_date': '2022-03-22',
-                                                                  'collaborators': '1, 2', 'is_finished': False}).json())
+print('4:', requests.get('http://127.0.0.1:8080/api/jobs/etbt').json())
+print('5:', requests.post('http://127.0.0.1:8080/api/jobs/',
+                          json={'id': 5, 'team_leader': 1, 'job': 'w1', 'work_size': 20,
+                                'start_date': dt.datetime.now().date().isoformat(),
+                                'end_date': dt.date(year=2023, month=5, day=1).isoformat(),
+                                'is_finished': False}).json())
